011e9de16c58ac92b549fa43082027b631a059de
jiangxiaobai <jiangxiaobai1989@gmail.com>
2016-08-02 16:24:15 +0000

    新增添加设备功能

--- demo01.py
+++ demo01.py
@@ -5,10 +5,18 @@ from OneNetApi import *
 
 
 if __name__ == '__main__':
-    test = OneNetApi("Your Api Key")
+    test = OneNetApi("Ur api key")
+
+    # 添加设备
+    title = "test11"
+    desc = "testing"
+    tags = ["test1", "test2"]
+    private = "false"
+    res1 = test.device_add(title = title, desc = desc, tags = tags, private = private)
+    print res1.content
 
     # 添加数据
-    device_id = *******
+    #device_id = *****
     #无名参数要放在keyword前面
     #res1 = test.datapoint_add(device_id, 'temperature', 70)
     #print res1.content
@@ -17,11 +25,11 @@ if __name__ == '__main__':
 
     # 获取数据
     # stream_id使用list
-    datastream_ids = ['temperature', 'humidity']
+    #datastream_ids = ['temperature', 'humidity']
     #print type(datastream_ids)
-    start_time = "2016-07-30 00:00:01"
-    end_time = "2016-07-31 23:59:59"
-    limit = 100
-    res3 = test.datapoint_multi_get(device_id = device_id, start_time = start_time, end_time = end_time, limit = limit, datastream_ids = datastream_ids)
-    print res3.content
+    #start_time = "2016-07-30 00:00:01"
+    #end_time = "2016-07-31 23:59:59"
+    #limit = 100
+    #res3 = test.datapoint_multi_get(device_id = device_id, start_time = start_time, end_time = end_time, limit = limit, datastream_ids = datastream_ids)
+    #print res3.content
 
